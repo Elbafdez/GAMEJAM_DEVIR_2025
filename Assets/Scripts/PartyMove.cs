fe197e795c856c51563c51eb6f12b9b143f9151e
Elba <elba.fdez.lpz@gmail.com>
2025-06-09 17:34:06 +0200

    Gameplay

--- Assets/Scripts/PartyMove.cs
+++ Assets/Scripts/PartyMove.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class PartyMove : MonoBehaviour
 {
     public float velocidad = 3f;
+    public float escalaPorPersona;
+    public float escalaPorBorracho;
+    public float escalaMinima;
+    public float escalaMaxima;
 
     void Update()
     {
@@ -20,5 +24,33 @@ public class PartyMove : MonoBehaviour
 
         // Aplicar movimiento al GameObject
         transform.Translate(movimiento * velocidad * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Persona"))
+        {
+            // Aumentar escala
+            CambiarEscala(escalaPorPersona);
+            GameManager.Instance.RemoverPersona((Vector2)other.transform.position);
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Borracho"))
+        {
+            // Disminuir escala
+            CambiarEscala(-escalaPorBorracho);
+            GameManager.Instance.RemoverPersona((Vector2)other.transform.position);
+            Destroy(other.gameObject);
+        }
+    }
+
+    void CambiarEscala(float delta)
+    {
+        // Cambiar la escala del GameObject
+        Vector3 nuevaEscala = transform.localScale + new Vector3(delta, delta, 0f);
+        // Asegurarse de que la escala se mantenga dentro de los límites
+        nuevaEscala.x = Mathf.Clamp(nuevaEscala.x, escalaMinima, escalaMaxima);
+        nuevaEscala.y = Mathf.Clamp(nuevaEscala.y, escalaMinima, escalaMaxima);
+        transform.localScale = nuevaEscala;
     }
 }
